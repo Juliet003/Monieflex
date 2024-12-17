3ed77da4368887eb4c79ad1b9fc3ce486841b237
juliet003 <julietadiela@gmail.com>
2024-12-17 15:04:52 +0100

    get all banks implemented

---
+++ src/main/java/com/example/monieflex/entities/Bank.java
@@ -0,0 +1,12 @@
+package com.example.monieflex.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Bank {
+    private int id;
+    private String code;
+    private String name;
+}
